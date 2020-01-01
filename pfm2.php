acae7679b5990e4482149c1d560ddab671c55cb6
manchonjul <59064453+manchonjul@users.noreply.github.com>
2020-01-01 15:37:17 -0500

    Update and rename pfm2.html to pfm2.php
    Fixed up this naming error and just threw in a basic template for some updating to be done, but so one can still understand the basic concept.

---
+++ pfm2.php
@@ -0,0 +1,26 @@
+<h1 style="color: #4485b8;">Basic pfm page</h1>
+<p><strong style="color: #000;">Basic usage:</strong>&nbsp;This is probably where one would add some tasteful graphics, a more clever title, and probably your affiliate referral links to the following banks, or related financial affiliate marketing programs.<br />This page apecifically, also, is a pfm page. This is where we add some more marketing techniues based on transactions.</p>
+<p>To get to this page, the value in the code "if ($_POST['$transactions']) = (true);" must have been satisfied, or an error would have been thrown.</p>
+<p>This is just a rough example</p>
+
+<table class="editorDemoTable" style="vertical-align: top;">
+<thead>
+<tr style="height: 23px;">
+<td style="height: 23px;" width="290px">Tranactions</td>
+ 
+
+</thead>
+<tbody>
+
+<tr style="height: 45px;">
+  <td>
+    <?php
+ 	// we will better code these reults when we buil, run and finish coding the repo.
+     ?>
+
+  </td>
+<tr style="background-color: rgba(0,0,0,0.1);"></tr>
+</tbody>
+</table>
+<hr />
+Advertisements based on transactional history is going to be added here
